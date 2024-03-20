00300de3a59912e222cbd4d35312e1a25d4cebaa
nayoung16 <nykim1016@sookmyung.ac.kr>
2024-03-20 22:07:50 +0900

    HelloServletRequest

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,10 +1,10 @@
 package hello.servlet.basic;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     //단축키 ctrl+o
     @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
         //단축기 soutv
         System.out.println("request = " + request);
